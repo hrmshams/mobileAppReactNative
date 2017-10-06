e22d0751035569a7c5b98d49c42dec1240cbc0eb
hamid3potter <hrm.shams@gmail.com>
2017-10-06 20:49:32 +0330

    pre problem fixed

--- components/CommonComponents.js
+++ components/CommonComponents.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
     StyleSheet,
     View ,
+    Text,
 } from 'react-native';
 
 import PropTypes from 'prop-types';
--- components/Tabs/searchTab.js
+++ components/Tabs/searchTab.js
@@ -4,6 +4,7 @@ import {
     Text,
     ScrollView,
     StyleSheet,
+    Image
 
 } from 'react-native';
 
@@ -68,40 +69,51 @@ const styles = StyleSheet.create({
 //
 // components of search tab!
 //
-
-export class SearchHeader extends Component{
+class SearchHeader extends React.Component{
     constructor(props){
         super(props);
 
+        this.first_text = "دنبال چی میگردی؟";
+        this.second_text = "ویلا یا خونه موردنظرتو جستجو کن!";
+
         this.style = StyleSheet.create({
             container : {
-                height: 50,
-                backgroundColor : 'blue',
+                // height: 150,
+                // backgroundColor : 'blue',
                 flex : 1,
                 flexDirection: 'row',
-                justifyContent: 'flex-start',
+                justifyContent: 'flex-end',
                 alignItems:'center',
             },
             searchImage : {
-
+                width : 64,
+                height : 64,
+                margin : 30,
+                marginBottom : 20,
+                marginLeft :20,
             },
             text : {
-
+                fontFamily: "IRANYekanMobileBold",
+                fontSize : 19,
+                color : '#454353',
             }
         });
-
     }
+
     render(){
         return(
             <View style={this.style.container}>
-                <image
-                source = {{uri:'./../Images/search.png'}}
-                style={this.style.searchImage}
+                <View>
+                    <Text style={this.style.text}>{this.first_text}</Text>
+                    <Text style={this.style.text}>{this.second_text}</Text>
+                </View>
+                <Image
+                    source = {require('./../Images/search.png')}
+                    style={this.style.searchImage}
                 />
-
-                <Text>test</Text>
 
             </View>
         );
     }
 }
+
